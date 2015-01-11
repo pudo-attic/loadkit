e10b0e89ae8e4a74ee6159214668a5e8761ff767
Friedrich Lindenberg <friedrich@pudo.org>
2015-01-11 12:59:09 +0100

    Add timestamps.

--- loadkit/core/manifest.py
+++ loadkit/core/manifest.py
@@ -1,5 +1,6 @@
 import json
 import collections
+from datetime import datetime
 
 
 from loadkit.util import json_default, json_hook
@@ -16,9 +17,11 @@ class Manifest(dict):
         if self.key.exists():
             self.update(json.load(self.key, object_hook=json_hook))
         else:
+            self['created_at'] = datetime.utcnow()
             self.update({'resources': {}})
 
     def save(self):
+        self['updated_at'] = datetime.utcnow()
         content = json.dumps(self, default=json_default, indent=2)
         self.key.set_contents_from_string(content)
 
@@ -36,16 +39,24 @@ class ResourceMetaData(collections.MutableMapping):
             self.manifest['resources'] = {}
         existing = self.manifest['resources'].get(self.resource.path)
         if not isinstance(existing, dict):
-            self.manifest['resources'][self.resource.path] = {}
+            self.manifest['resources'][self.resource.path] = {
+                'created_at': datetime.utcnow()
+            }
+
+    def touch(self):
+        self.manifest['resources'][self.resource.path]['updated_at'] = \
+            datetime.utcnow()
 
     def __getitem__(self, key):
         return self.manifest['resources'][self.resource.path][key]
 
     def __setitem__(self, key, value):
         self.manifest['resources'][self.resource.path][key] = value
+        self.touch()
 
     def __delitem__(self, key):
         del self.manifest['resources'][self.resource.path][key]
+        self.touch()
 
     def __iter__(self):
         return iter(self.manifest['resources'][self.resource.path])
@@ -57,6 +68,7 @@ class ResourceMetaData(collections.MutableMapping):
         return key
 
     def save(self):
+        self.touch()
         self.resource.package.save()
 
     def __repr__(self):
