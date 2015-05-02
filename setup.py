51c61d023dea22fa28701e494c53f9b1a9a9ebee
Friedrich Lindenberg <friedrich@pudo.org>
2015-05-02 20:18:34 +0200

    up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='loadkit',
-    version='0.3.2',
+    version='0.3.3',
     description="Light-weight tools for ETL",
     long_description="",
     classifiers=[
